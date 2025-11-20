b4075ff7f4ec9834bc82ede8fb20bede30067b40
GreyF <greysforgettea@gmail.com>
2025-11-20 17:45:46 +0700

    подключили необходимые библиотеки и добавили доп папки для структуры проекта

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 import './style.css'
 import App from './App.vue'
+import router from './router'
 import "./scss/main.scss";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
